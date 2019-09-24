72b1edeb335b5be1f8365f4e1fa04f5ae1e2922a
Paolo Pisati <paolo.pisati@canonical.com>
2019-09-24 11:01:46 +0200

    UBUNTU: [Debian] package bpftool in linux-tools-common
    BugLink: https://bugs.launchpad.net/bugs/1774815
    
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- debian.gcp/rules.d/amd64.mk
+++ debian.gcp/rules.d/amd64.mk
@@ -15,6 +15,7 @@ do_tools_cpupower = true
 do_tools_perf   = true
 do_tools_x86	= true
 do_tools_hyperv	= false
+do_tools_bpftool = true
 do_extras_package = true
 do_tools_common = true
 do_zfs		= true
