ecc4200b118a69924ce9bd00accde20af718b0b8
Brijesh Singh <brijesh.singh@amd.com>
2022-01-26 09:42:11 -0600

    x86/sev: Register GHCB memory when SEV-SNP is active
    BugLink: http://bugs.launchpad.net/bugs/2001605
    
    The SEV-SNP guest is required by the GHCB spec to register the GHCB's
    Guest Physical Address (GPA). This is because the hypervisor may prefer
    that a guest use a consistent and/or specific GPA for the GHCB associated
    with a vCPU. For more information, see the GHCB specification section
    "GHCB GPA Registration".
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    (cherry picked from commit 95d33bfaa3e169cfec1926e0d0f0c6b0ea75d763)
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -59,6 +59,7 @@
 #include <asm/cpu_device_id.h>
 #include <asm/uv/uv.h>
 #include <asm/sigframe.h>
+#include <asm/sev.h>
 
 #include "cpu.h"
 
@@ -2136,6 +2137,9 @@ void cpu_init_exception_handling(void)
 	set_tss_desc(cpu, &get_cpu_entry_area(cpu)->tss.x86_tss);
 
 	load_TR_desc();
+
+	/* GHCB need to be setup to handle #VC. */
+	setup_ghcb();
 
 	/* Finally load the IDT */
 	load_current_idt();
