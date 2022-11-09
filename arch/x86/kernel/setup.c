7ed7725778f2104ad4466003309cbd8b5f5cb7b4
Ashish Kalra <ashish.kalra@amd.com>
2022-11-09 14:06:52 -0300

    x86, swiotlb: Adjust SWIOTLB bounce buffer size for SEV guests
    BugLink: https://bugs.launchpad.net/bugs/1958416
    
    For SEV, all DMA to and from guest has to use shared (un-encrypted) pages.
    SEV uses SWIOTLB to make this happen without requiring changes to device
    drivers.  However, depending on the workload being run, the default 64MB
    of it might not be enough and it may run out of buffers to use for DMA,
    resulting in I/O errors and/or performance degradation for high
    I/O workloads.
    
    Adjust the default size of SWIOTLB for SEV guests using a
    percentage of the total memory available to guest for the SWIOTLB buffers.
    
    Adds a new sev_setup_arch() function which is invoked from setup_arch()
    and it calls into a new swiotlb generic code function swiotlb_adjust_size()
    to do the SWIOTLB buffer adjustment.
    
    v5 fixed build errors and warnings as
    Reported-by: kbuild test robot <lkp@intel.com>
    
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Co-developed-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    (backported from commit e998879d4fb7991856916972168cf27c0d86ed12)
    [cascardo: conflict fixup due to 064ce6c550a0630789978bfec7a13ab2bd1bdcdf backport]
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Luke Nowakowski-Krijger <luke.nowakowskikrijger@canonical.com

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1128,6 +1128,12 @@ void __init setup_arch(char **cmdline_p)
 	memblock_set_current_limit(ISA_END_ADDRESS);
 	e820__memblock_setup();
 
+	/*
+	 * Needs to run after memblock setup because it needs the physical
+	 * memory size.
+	 */
+	sev_setup_arch();
+
 	reserve_bios_regions();
 
 	if (efi_enabled(EFI_MEMMAP)) {
