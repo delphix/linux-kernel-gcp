svsm-vtpm/linux

x86/compressed/acpi: Move EFI config table lookup to helper
Future patches for SEV-SNP-validated CPUID will also require early
parsing of the EFI configuration. Incrementally move the related code
into a set of helpers that can be re-used for that purpose.

Signed-off-by: Michael Roth <michael.roth@amd.com>
Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

arch/x86/boot/compressed/misc.h
@@ -186,6 +186,8 @@ enum efi_type {
 /* helpers for early EFI config table access */
 enum efi_type efi_get_type(struct boot_params *boot_params);
 unsigned long efi_get_system_table(struct boot_params *boot_params);
+int efi_get_conf_table(struct boot_params *boot_params, unsigned long *cfg_tbl_pa,
+		       unsigned int *cfg_tbl_len);
 #else
 static inline enum efi_type efi_get_type(struct boot_params *boot_params)
 {
@@ -195,6 +197,13 @@ static inline enum efi_type efi_get_type(struct boot_params *boot_params)
 static inline unsigned long efi_get_system_table(struct boot_params *boot_params)
 {
 	return 0;
+}
+
+static inline int efi_get_conf_table(struct boot_params *boot_params,
+				     unsigned long *cfg_tbl_pa,
+				     unsigned int *cfg_tbl_len)
+{
+	return -ENOENT;
 }
 #endif /* CONFIG_EFI */
 

delphix/linux-kernel-gcp

BugLink: http://bugs.launchpad.net/bugs/2001605
(cherry picked from commit 61c14ceda8405f37545a0983d6b9bc45c6236793)
Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>
Acked-by: Tim Gardner <tim.gardner@canonical.com>
Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>

arch/x86/boot/compressed/misc.h
@@ -183,6 +183,8 @@ enum efi_type {
 /* helpers for early EFI config table access */
 enum efi_type efi_get_type(struct boot_params *bp);
 unsigned long efi_get_system_table(struct boot_params *bp);
+int efi_get_conf_table(struct boot_params *bp, unsigned long *cfg_tbl_pa,
+		       unsigned int *cfg_tbl_len);
 #else
 static inline enum efi_type efi_get_type(struct boot_params *bp)
 {
@@ -192,6 +194,13 @@ static inline enum efi_type efi_get_type(struct boot_params *bp)
 static inline unsigned long efi_get_system_table(struct boot_params *bp)
 {
 	return 0;
+}
+
+static inline int efi_get_conf_table(struct boot_params *bp,
+				     unsigned long *cfg_tbl_pa,
+				     unsigned int *cfg_tbl_len)
+{
+	return -ENOENT;
 }
 #endif /* CONFIG_EFI */
 