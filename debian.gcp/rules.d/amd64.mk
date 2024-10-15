779126dc733651663c12789c17df01c8e28ca57b
Kevin Becker <kevin.becker@canonical.com>
2024-10-15 11:37:57 -0400

    UBUNTU: [Packaging]: gcp: Don't produce linux-*-cloud-tools-common, linux-*-tools-common and linux-*-tools-host binary packages
    BugLink: https://bugs.launchpad.net/ubuntu/+source/linux/+bug/2048183
    
    Signed-off-by: Kevin Becker <kevin.becker@canonical.com>

--- debian.gcp/rules.d/amd64.mk
+++ debian.gcp/rules.d/amd64.mk
@@ -17,7 +17,6 @@ do_tools_x86	= true
 do_tools_hyperv	= false
 do_tools_bpftool = true
 do_extras_package = true
-do_tools_common = true
 do_zfs		= true
 disable_d_i	= true
 do_source_package = false
