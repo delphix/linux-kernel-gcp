d439e882b163bbc3380f54d534263214aad54d5a
Kevin Becker <kevin.becker@canonical.com>
2024-11-20 19:56:39 -0500

    UBUNTU: [Packaging] gcp: Remove do_source_package
    BugLink: https://bugs.launchpad.net/ubuntu/+source/linux/+bug/2048183
    
    Signed-off-by: Kevin Becker <kevin.becker@canonical.com>

--- debian.gcp/rules.d/amd64.mk
+++ debian.gcp/rules.d/amd64.mk
@@ -19,7 +19,6 @@ do_tools_bpftool = true
 do_extras_package = true
 do_zfs		= true
 disable_d_i	= true
-do_source_package = false
 do_dtbs		= false
 do_common_headers_indep = false
 do_dkms_nvidia  = true
